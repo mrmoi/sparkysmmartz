fe997099b00c03fead523b9ca053f69215d74b68
mrmoi <mlmart16@asu.edu>
2017-12-30 16:57:51 -0700

    first css customsization

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-fixed-bottom customFooter">
+    <ul>
+        <li>Copyright &copy; 2016 Sparky's Coffee House |</li>
+        <li>231 University Dr.</li>
+        <li>Tempe, AZ 85281 |</li>
+        <li><a id=”mobile” href=”tel:480-555-5555”><span id=”desktop”>480-555-5555</span></a></li>
+    </ul>
+</nav>
